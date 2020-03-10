1186f6b496ae124a6607a982310d172bf8fef7cf
Adriana <almazan624@gmail.com>
2020-03-10 13:28:38 -0600

    Faltan cuenta y amigos, ya tiene navegacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@Angular/router';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
+import { CuentaComponent } from './cuenta/cuenta.component';
+import { AmigosComponent } from './amigos/amigos.component';
+
+const routes: Routes = [
+  { path: 'cuenta', component: CuentaComponent },
+  { path: 'amigos', component: AmigosComponent },
+  { path: '**', redirectTo: '/', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    CuentaComponent,
+    AmigosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
